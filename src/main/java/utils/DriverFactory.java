4e73861cca5295d394c3152a864c67d6f8278aec
laylo-botirova <botirovalaylo567@gmail.com>
2026-04-01 04:53:30 -0700

    Update on DriverFactory

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -4,16 +4,27 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
     public static WebDriver getDriver(String browser){
         if(browser.equalsIgnoreCase("chrome")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
             WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
+            return new ChromeDriver(options);
+
         } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            return new FirefoxDriver();
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+
+            return new FirefoxDriver(options);
         }
         throw new RuntimeException("unsupported browser");
     }
